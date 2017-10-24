4d071e55e772432e696d8a6761fe20d9756ef220
ygorcf <ygorcruzfelix@gmail.com>
2017-10-24 11:16:02 -0200

    Comecado telas #1

---
+++ src/android/IluminaTI/app/src/main/java/com/example/ygor/iluminati/JogoActivity.java
@@ -0,0 +1,13 @@
+package com.example.ygor.iluminati;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo);
+    }
+}
