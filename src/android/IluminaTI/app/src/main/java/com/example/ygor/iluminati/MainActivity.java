51c42a69f9e4856140d1000f118876649aa9344a
ygorcf <ygorcruzfelix@gmail.com>
2017-10-25 00:06:59 -0200

    Adicionados Frameworks necessarios

--- src/android/IluminaTI/app/src/main/java/com/example/ygor/iluminati/MainActivity.java
+++ src/android/IluminaTI/app/src/main/java/com/example/ygor/iluminati/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ygor.iluminati;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btnCheckin)
+    public void checkin() {
+        Intent i = new Intent(this, EventoActivity.class);
+        startActivityForResult(i, 100);
+    }
+
 }
