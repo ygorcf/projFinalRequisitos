e0272033203cdb256e22ad57254d7b672a211405
ygorcf <ygorcruzfelix@gmail.com>
2017-11-07 09:56:17 -0200

    Criado API com as rotas necessarias

---
+++ src/server/miniucb/models/palestras.js
@@ -0,0 +1,22 @@
+var palestras = [
+  {
+    nome: 'Abertura',
+    horario: '08:30',
+    dia: new Date(2017, 08, 27),
+    alunosCheckin: []
+  },
+  {
+    nome: 'Segurança da Informação',
+    horario: '19:30',
+    dia: new Date(2017, 08, 27),
+    alunosCheckin: []
+  },
+  {
+    nome: 'Papo de banco de dados',
+    horario: '19:30',
+    dia: new Date(2017, 08, 29),
+    alunosCheckin: []
+  }
+]
+
+module.exports = palestras
---
+++ src/server/miniucb/routes/v1/usuario/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+const responseHelper = require('../../../helpers/responseHelper')
+
+var router = express.Router();
+
+/* POST login. */
+router.post('/login', function(req, res, next) {
+  var matricula = req.body.matricula
+  if (matricula.match(/^UC[0-9]{8}/) !== null) {
+    res.status(200).json(responseHelper(true, 200, {
+      logged: true,
+      matricula: matricula,
+      userType: 'a'
+    }))
+  } else if (matricula.match(/^(UC)?[0-9]{6}/) !== null) {
+    res.status(200).json(responseHelper(true, 200, {
+      logged: true,
+      matricula: matricula,
+      userType: 'd'
+    }))
+  } else if (matricula.match(/.*@.*\.com/) !== null) {
+    res.status(200).json(responseHelper(true, 200, {
+      logged: true,
+      matricula: matricula,
+      userType: 'p'
+    }))
+  } else {
+    res.status(404).json(responseHelper(false, 404, {
+      logged: false,
+      error: 'Matricula invalida.'
+    }))
+  }
+});
+
+module.exports = router;
