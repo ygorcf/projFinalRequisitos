8db5ad3e50892c1acf13296fb3964d002fed2648
ygorcf <ygorcruzfelix@gmail.com>
2017-11-13 10:49:46 -0200

    Corrigidos locais dos arquivos e feito caso de uso de apresentar cronograma

--- src/server/miniucb/models/palestras.js
+++ src/server/miniucb/models/palestras.js
@@ -2,19 +2,19 @@ var palestras = [
   {
     nome: 'Abertura',
     horario: '08:30',
-    dia: new Date(2017, 08, 27),
+    dia: new Date(2017, 10, 22).getTime(),
     alunosCheckin: []
   },
   {
     nome: 'Segurança da Informação',
     horario: '19:30',
-    dia: new Date(2017, 08, 27),
+    dia: new Date(2017, 10, 22).getTime(),
     alunosCheckin: []
   },
   {
     nome: 'Papo de banco de dados',
     horario: '19:30',
-    dia: new Date(2017, 08, 29),
+    dia: new Date(2017, 10, 24).getTime(),
     alunosCheckin: []
   }
 ]
