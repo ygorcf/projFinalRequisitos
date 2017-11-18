5ba18f40042863348146d72e06abec1c281e7d77
ygorcf <ygorcruzfelix@gmail.com>
2017-11-18 00:38:42 -0200

    Criado paginas para mostrar as urls da api

--- src/server/miniucb/routes/v1/index.js
+++ src/server/miniucb/routes/v1/index.js
@@ -4,4 +4,14 @@ var router = express.Router();
 router.use('/palestra', require('./palestra'))
 router.use('/usuario', require('./usuario'))
 
+/* GET generates list routes. */
+router.get('/', function(req, res, next) {
+  res.render('routes', { 
+    routes: [
+      require('./palestra').stack.map(v => v.route ? {path: req.originalUrl + '/palestra' + v.route.path, method: v.route.methods} : {}),
+      require('./usuario').stack.map(v => v.route ? {path: req.originalUrl + '/usuario' + v.route.path, method: v.route.methods} : {}),
+    ]
+  });
+})
+
 module.exports = router;
